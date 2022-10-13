39cd7a13c285a19cf49fb4bd3858ecadca4d360a
jeebx <jeeb24@gmail.com>
2022-10-13 19:19:22 -0400

    add rock pictures, names

--- pages/api/rocks/index.jsx
+++ pages/api/rocks/index.jsx
@@ -6,17 +6,14 @@ const chunked = chunk(RockData, 200)
 // const apiKey = process.env.OPENSEA_API_KEY
 
 const fetchRockPage = async (ids) => {
-  let url = 'https://opensea.io/assets/ethereum/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/'
+  let url;
   let rockIdList = [];
-  let rockNameList = [];
   let rockIds = Object.keys(RockData[0]);
   let rockNames = Object.values(RockData[0]);
-  
-  for(let i=0;i<rockIds.length;i++) {
-    url += `${rockIds[i]}`;
-    rockIdList.push(url);
-    rockNameList.push(rockNames[i].name)
-    url = 'https://opensea.io/assets/ethereum/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/';
+
+  for (let i = 0; i < rockIds.length; i++) {
+    url = `https://opensea.io/assets/ethereum/0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85/${rockIds[i]}`;
+    rockIdList.push({ url: url, name: rockNames[i].name });
   }
 
   return rockIdList;
@@ -25,7 +22,6 @@ const fetchRockPage = async (ids) => {
 export const fetchRocks = async () => {
   const data = await pMap(chunked, fetchRockPage, { concurrency: 2 })
   const mapped = flatten(data)
-  // console.log(mapped);
   return mapped;
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { fetchRocks, RockInfo } from './api/rocks'
+import { fetchRocks } from './api/rocks'
 import { format as ts } from 'timeago.js'
 
 export async function getStaticProps() {
@@ -9,12 +9,11 @@ export async function getStaticProps() {
 const Rock = ({ rock }) => {
 
   return (
-    <a href={rock} target="_blank">
+    <a href={rock.url} target="_blank">
       <div className="m-auto pb-4 mb-8 flex flex-col justify-center items-center gap-2 p-4 md:m-4 border border-white transform hover:scale-105 transition-all bg-black w-full md:w-96">
-        <img src='baseRock.jpg' alt="" width="350" height="350" />
+        <img style={{ width: "200px", height: "200px" }} src={`/images/${rock.name}.jpeg`} width="350" height="350" />
         <div className="text-center">
-          <p className="text-lg">#{rock.id}</p>
-          <p>{rock.price} ETH</p>
+          <p className="text-lg">{rock.name}</p>
         </div>
       </div>
     </a>
@@ -22,13 +21,19 @@ const Rock = ({ rock }) => {
 }
 
 const IndexPage = ({ data, lastUpdate }) => {
+  console.log(data);
   return (
     <div className="py-3 md:pb-0 font-mono flex flex-col justify-center items-center gap-4 pt-10 md:w-screen">
-      <h1 className="text-lg md:text-3xl">ENS Rocks</h1>
+      <h1 className="text-lg md:text-3xl">ENS Rock DAO</h1>
       <div className="text-center max-w-screen-md md:leading-loose">
         <p className="md:text-xl">
-          There are only 5 ENS names for sale. The floor
-          price is 200 ETH.
+          Rocks compiled from{' '}
+          <a
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/List_of_rock_types"
+            className="underline">
+            wikipedia
+          </a>
         </p>
         <p className="md:text-lg pt-2">
           Site by{' '}
@@ -39,10 +44,10 @@ const IndexPage = ({ data, lastUpdate }) => {
           >
             swftbb
           </a>
-        .
+          .
         </p>
         <p>
-        Inspired by{' '}
+          Inspired by{' '}
           <a
             target="_blank"
             href="https://twitter.com/worm_emoji"
@@ -50,7 +55,7 @@ const IndexPage = ({ data, lastUpdate }) => {
           >
             worm_emoji
           </a>
-           {' '} and {' '}
+          {' '} and {' '}
           <a
             target="_blank"
             href="https://robes.market/"
@@ -62,8 +67,8 @@ const IndexPage = ({ data, lastUpdate }) => {
         <p className="text-sm mv-4">Last updated {ts(lastUpdate)}</p>
       </div>
       <div className="grid md:grid-cols-3 pt-5">
-        {data.map((rock) => {
-          return <Rock rock={rock} key={rock.id} />
+        {data.map((rock, index) => {
+          return <Rock rock={rock} key={index} />
         })}
       </div>
     </div>
